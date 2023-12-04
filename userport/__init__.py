8e8e6890364c153d9a1e941da715327e2b2375fd
Addarsh <addarsh@userport.ai>
2023-12-04 16:41:42 +0530

    Added CSRF protection for POST requests

--- userport/__init__.py
+++ userport/__init__.py
@@ -1,8 +1,11 @@
 from . import auth
 import os
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
+
+csrf = CSRFProtect()
 
 
 def create_app():
@@ -13,5 +16,6 @@ def create_app():
         MONGO_DB_NAME='db'
     )
     app.register_blueprint(auth.bp)
+    csrf.init_app(app)
 
     return app
